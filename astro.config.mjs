16f58eac870183a358e8105baae510e3069ea917
Russ Brenner <5236354+russbrenner@users.noreply.github.com>
2025-08-14 01:23:02 +1000

    Simplify landing page to 'Briefer - Coming Soon...'

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://briefer.pages.dev",
+  site: "https://briefer.me",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
