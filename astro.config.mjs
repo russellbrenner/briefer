b2e028540307fa2ff17807c4a3a74042877c3263
Russ Brenner <5236354+russbrenner@users.noreply.github.com>
2025-08-14 11:38:28 +1000

    Fix deployment: switch to static build and Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://briefer.me",
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  output: "static"
 });
